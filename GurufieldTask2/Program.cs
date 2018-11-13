4b63f7ba240df6022f554a4092b93b1c12589f87
Kornylevych Andriy <kornelandriy@gmail.com>
2018-11-13 23:54:27 +0200

    Implemented task #2

---
+++ GurufieldTask2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace GurufieldTask2
+{
+    class Program
+    {
+        static void Main()
+        {
+            var array = Enumerable.Range(1, 100000)
+                .OrderBy(item => new Random().Next())
+                .ToArray();
+        }
+    }
+}
